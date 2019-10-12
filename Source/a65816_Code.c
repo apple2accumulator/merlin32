23efb358c226007211b2b96bd9197db67a2106a0
Lane Roathe <lane.roathe@quicken.com>
2019-10-11 18:30:53 -0700

    Code cleanup

--- Source/a65816_Code.c
+++ Source/a65816_Code.c
@@ -2636,8 +2636,7 @@ static int DecodeAddressMode(struct source_line *current_line, char *error_buffe
 /***************************************************************************************************************/
 static int GetOperandNbByte(char *operand, struct source_line *current_line, int *is_address_rtn, char *buffer_error_rtn, struct omf_segment *current_omfsegment)
 {
-    int nb_element = 0, nb_max_byte = 0, nb_byte = 0, bit_mode = 0, value_format = 0, has_extra_hash = 0;
-    int has_hash = 0, has_less = 0, has_more = 0, has_exp = 0, has_pipe = 0, has_long_addr = 0, is_block_copy = 0;
+    int nb_element = 0, nb_max_byte = 0, nb_byte = 0, bit_mode = 0, value_format = 0, has_extra_hash = 0, has_long_addr = 0, is_block_copy = 0;
     int is_address = 1;
     char **tab_element = NULL;
     strcpy(buffer_error_rtn,"");
@@ -2657,12 +2656,12 @@ static int GetOperandNbByte(char *operand, struct source_line *current_line, int
     if(!my_stricmp(current_line->opcode_txt,"MVN") || !my_stricmp(current_line->opcode_txt,"MVP"))
         is_block_copy = 1;
     
-    /** Process the # <> ^ | **/
-    has_hash = (operand[0] == '#') ? 1 : 0;
-    has_less = (operand[has_hash] == '<') ? 1 : 0;
-    has_more = (operand[has_hash] == '>') ? 1 : 0;
-    has_exp = (operand[has_hash] == '^') ? 1 : 0;
-    has_pipe = (operand[has_hash] == '|' || operand[has_hash] == '!') ? 1 : 0;
+    /** Process the # < > ^ | **/
+    int has_hash = (operand[0] == '#') ? 1 : 0;
+    int has_less = (operand[has_hash] == '<') ? 1 : 0;
+    int has_more = (operand[has_hash] == '>') ? 1 : 0;
+    int has_exp = (operand[has_hash] == '^') ? 1 : 0;
+    int has_pipe = (operand[has_hash] == '|' || operand[has_hash] == '!') ? 1 : 0;
     
     /** Cut the string of characters into several elements (skips the #> <^ | from the beginning) **/
     tab_element = DecodeOperandeAsElementTable(&operand[has_hash+has_less+has_more+has_exp+has_pipe],&nb_element,SEPARATOR_EVALUATE_EXPRESSION,current_line);
@@ -2764,7 +2763,7 @@ static int GetOperandNbByte(char *operand, struct source_line *current_line, int
     /* Memory release */
     mem_free_table(nb_element,tab_element);
     
-    /* Is the Operand an address? (Labl1 + 1 is an address, (Label1-Label2) is a value, #toto is a value) */
+    /* Is the Operand an address? (#anything is a value, everything else is an address) */
     if(has_hash == 1)
         is_address = 0;
     
